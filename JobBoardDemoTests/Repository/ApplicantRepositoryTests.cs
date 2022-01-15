8f5652936ebb510a84b56d85c20544a0454322b2
xandrf <xandrf@protonmail.com>
2022-01-15 18:32:48 -0300

    ApplicantServiceTests.cs Done

---
+++ JobBoardDemoTests/Repository/ApplicantRepositoryTests.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Bogus;
+using FluentAssertions;
+using JobBoardDemoApi.Controllers;
+using JobBoardDemoApi.Controllers.Interface;
+using JobBoardRepository;
+using JobBoardRepository.Domain;
+using JobBoardRepository.Interface;
+using JobBoardServices.Interface;
+using JobBoardServices.Profile;
+using JobBoardServices.View;
+using Moq;
+using Xunit;
+
+namespace JobBoardDemoTests.Repository;
+
+public class ApplicantRepositoryTests
+{
+    public Mock<IDapperWrapper> _dw;
+    public Mock<ISqlReader> _sqlreader;
+    public ApplicantRepository _jr;
+    public Faker<ApplicantDTO> faker;
+    
+    public ApplicantRepositoryTests()
+    {
+        _dw = new Mock<IDapperWrapper>();
+        _sqlreader = new Mock<ISqlReader>();
+        _jr = new ApplicantRepository(_dw.Object,_sqlreader.Object);
+        faker = new Faker<ApplicantDTO>()
+            .RuleFor<int>(property:x=>x.Id,setter:y=>y.IndexFaker)
+            .RuleFor<string>(property:x=>x.Name,setter:y=>y.Name.FullName())
+            .RuleFor<double>(property:x=>x.WageExpectation,setter:y=>y.Finance.Random.Double());
+    }
+    [Fact]
+    public async void OnGet_ValidValue_ShouldReturnObject()
+    {
+        //Arrange
+        var data = faker.Generate(20).ToArray();
+        _dw.Setup(x => x.QueryAsync<ApplicantDTO>(It.IsAny<string>()))
+            .ReturnsAsync(data);;
+        _sqlreader.Setup(x => x.ReadFile(It.IsAny<string>()))
+            .Returns<string>(async x => "abc");
+        //Act   
+        var result = await _jr.Get();
+        //Assert
+        result.Should().BeEquivalentTo(data);
+    }
+    [Fact]
+    public async void OnGet_EmptyValue_ShouldReturnEmptyArray()
+    {
+        //Arrange
+        _dw.Setup(x => x.QueryAsync<ApplicantDTO>(It.IsAny<string>()))
+            .ReturnsAsync(Array.Empty<ApplicantDTO>());
+        _sqlreader.Setup(x => x.ReadFile(It.IsAny<string>()))
+            .ReturnsAsync("abc");
+        //Act   
+        var result = await _jr.Get();
+        //Assert
+        result.Should().BeEmpty();
+    }
+    [Fact]
+    public async void OnPost_ValidValue_ShouldReturnTrue()
+    {
+        //Arrange
+        var data = faker.Generate(1).First();
+        _dw.Setup(x => x.ExecuteParamsAsync(It.IsAny<string>(), It.IsAny<object>()));
+        _sqlreader.Setup(x => x.ReadFile(It.IsAny<string>()))
+            .ReturnsAsync("abc");
+        //Act   
+        var result = await _jr.Post(data);
+        //Assert
+        result.Should().BeTrue();
+        
+    }
+    [Fact]
+    public async void OnPost_InvalidValue_ShouldReturnFalse()
+    {
+        //Arrange
+        _dw.Setup(x => x.ExecuteParamsAsync<ApplicantDTO>(It.IsAny<string>(),It.IsAny<ApplicantDTO>()))
+            .Throws<Exception>();
+        _sqlreader.Setup(x => x.ReadFile(It.IsAny<string>()))
+            .ReturnsAsync("abc");
+        //Act   
+        bool result = await _jr.Post(null);
+        //Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public async void OnPost_ValidValue_ShouldReturnMappedObject()
+    {
+        //Arrange
+        var data = faker.Generate(1)[0];
+        _dw.Setup(x => x.ExecuteParamsAsync<ApplicantDTO>(It.IsAny<string>(), It.IsAny<ApplicantDTO>()));
+        var result = await _jr.Post(data);
+        //Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public async void OnGetId_ValidId_ShouldReturnMappedObject()
+    {
+        //Arrange
+        var data = faker.Generate(1);
+        _dw.Setup(x => x.QueryAsyncParams<ApplicantDTO>(It.IsAny<string>(),It.IsAny<object>()))
+            .ReturnsAsync(data);
+        _sqlreader.Setup(x => x.ReadFile(It.IsAny<string>()))
+            .Returns<string>(async x => "abc");
+        //Act   
+        ApplicantDTO result = await _jr.GetId(1);
+        //Assert
+        result.Should().BeEquivalentTo(data.ToList()[0]);
+    }
+
+    [Fact]
+    public async void OnGetId_InvalidId_ShouldThrowError()
+    {
+        //Arrange
+        _dw.Setup(x => x.QueryAsyncParams<ApplicantDTO>(It.IsAny<string>(), It.IsAny<object>()))
+            .ReturnsAsync(Array.Empty<ApplicantDTO>());
+        _sqlreader.Setup(x => x.ReadFile(It.IsAny<string>()));
+        //Act   
+        //Assert
+        await Assert.ThrowsAsync<Exception>(() => _jr.GetId(1));
+    }
+    [Fact]
+    public async void OnPut_ValidValue_ShouldReturnTrue()
+    {
+        //Arrange
+        var data = faker.Generate(1).First();
+        _dw.Setup(x => x.ExecuteParamsAsync(It.IsAny<string>(), It.IsAny<object>()));
+        _sqlreader.Setup(x => x.ReadFile(It.IsAny<string>()))
+            .ReturnsAsync("abc");
+        //Act   
+        var result = await _jr.Put(data);
+        //Assert
+        result.Should().BeTrue();
+    }
+    [Fact]
+    public async void OnPut_InvalidValue_ShouldReturnFalse()
+    {
+        //Arrange
+        _dw.Setup(x => x.ExecuteParamsAsync<ApplicantDTO>(It.IsAny<string>(), It.IsAny<ApplicantDTO>()))
+            .Throws<Exception>();
+        _sqlreader.Setup(x => x.ReadFile(It.IsAny<string>()))
+            .Returns<string>(async x => "abc");
+        //Act   
+        bool result = await _jr.Put(null);
+        //Assert
+        Assert.False(result);
+    }
+    [Fact]
+    public async void OnDelete_ValidValue_ShouldReturnTrue()
+    {
+        //Arrange
+        _dw.Setup(x => x.ExecuteParamsAsync(It.IsAny<string>(), It.IsAny<object>()));
+        _sqlreader.Setup(x => x.ReadFile(It.IsAny<string>()))
+            .ReturnsAsync("abc");
+        //Act   
+        var result = await _jr.Delete(1);
+        //Assert
+        result.Should().BeTrue();
+    }
+    [Fact]
+    public async void OnDelete_InvalidValue_ShouldReturnFalse()
+    {
+        //Arrange
+        _dw.Setup(x => x.ExecuteParamsAsync(It.IsAny<string>(), It.IsAny<object>()))
+            .Throws<Exception>();
+        _sqlreader.Setup(x => x.ReadFile(It.IsAny<string>()))
+            .ReturnsAsync("abc");
+        //Act   
+        bool result = await _jr.Delete(1);
+        //Assert
+        Assert.False(result);
+    }
+ 
+
+
+}
--- JobBoardDemoTests/Services/ApplicantServiceTests.cs
+++ JobBoardDemoTests/Services/ApplicantServiceTests.cs
@@ -1,167 +1,226 @@
-﻿
-// using System.Linq;
-// using AutoMapper;
-// using Bogus;
-// using FluentAssertions;
-// using JobBoardRepository.Domain;
-// using JobBoardRepository.Interface;
-// using JobBoardServices.Interface;
-// using JobBoardServices.Profile;
-// using JobBoardServices.Service;
-// using Moq;
-// using Xunit;
-//
-// namespace JobBoardDemoTests.Services;
-//
-// public class ApplicantServiceTests
-// {
-//     private IApplicantService _js;
-//     public Mock<IApplicantRepository> _jr;
-//     public Mock<IJobApplicantRepository> _jar;
-//     public Faker<ApplicantDTO> faker;
-//     public Mapper m ;
-//     public ApplicantServiceTests()
-//     {
-//         
-//         ApplicantProfile myProfile = new ApplicantProfile();
-//         var configuration = new MapperConfiguration(cfg => cfg.AddProfile(myProfile));
-//         m = new Mapper(configuration);
-//         this._jr = new Mock<IApplicantRepository>();
-//         this._jar = new Mock<IJobApplicantRepository>();
-//         this._js = new ApplicantService(_jr.Object, m,_jar.Object);
-//         faker = new Faker<ApplicantDTO>()
-//             .RuleFor<int>(property:x=>x.Id,setter:y=>y.IndexFaker)
-//             .RuleFor<string>(property:x=>x.Name,setter:y=>y.Name.FullName())
-//             .RuleFor<double>(property:x=>x.WageExpectation,setter:y=>y.Finance.Random.Double());
-//     }
-//
-//     [Fact]
-//     public async void OnGet_ValidValue_ShouldReturnMappedObject()
-//     {
-//         //Arrange
-//         var data = faker.Generate(20).ToArray();
-//         _jr.Setup(x => x.Get()).ReturnsAsync(data);
-//         var mockresult = data.Select(x =>
-//         {
-//             return new Applicant()
-//             {
-//                 id = x.ProductId,
-//                 description = x.Description,
-//                 title = x.Title,
-//                 maxPay = x.MaxPay,
-//                 minPay = x.MinPay,
-//                 subTitle = x.SubTitle
-//             };
-//         }).ToArray();
-//         //Act   
-//         var result = await _js.Get();
-//         //Assert
-//         result.Should().BeEquivalentTo(mockresult);
-//     }
-//     [Fact]
-//     public async void OnPost_FalseValue_ShouldReturnFalse()
-//     {
-//         //Arrange
-//         _jr.Setup(x => x.Post(null)).ReturnsAsync(false);
-//         //Act   
-//         var result = _js.Post(null);
-//         //Assert
-//         Assert.False( await result);
-//     }
-//
-//     [Fact]
-//     public async void OnPost_ValidValue_ShouldReturnMappedObject()
-//     {
-//         ;
-//         //Arrange
-//         var data = faker.Generate(1)[0];
-//         _jr.Setup(x => x.Post(It.IsAny<ApplicantDTO>())).ReturnsAsync(true);
-//         var mockresult = new Applicant()
-//         {
-//             id = data.ProductId,
-//             description = data.Description,
-//             title = data.Title,
-//             maxPay = data.MaxPay,
-//             minPay = data.MinPay,
-//             subTitle = data.SubTitle
-//         };
-//         //Act   
-//         var result = _js.Post(mockresult);
-//         //Assert
-//         Assert.True(await result);
-//     }
-//
-//     [Fact]
-//     public async void OnGetId_ValidId_ShouldReturnMappedObject()
-//     {
-//         //Arrange
-//         var data = faker.Generate(1)[0];
-//         _jr.Setup(x => x.GetId(1)).ReturnsAsync(data);
-//         var mockresult = new Applicant()
-//         {
-//             id = data.ProductId,
-//             description = data.Description,
-//             title = data.Title,
-//             maxPay = data.MaxPay,
-//             minPay = data.MinPay,
-//             subTitle = data.SubTitle
-//         };
-//         //Act   
-//         var result = await _js.GetId(1);
-//         //Assert
-//         result.Should().BeEquivalentTo(mockresult);
-//     }
-//     [Fact]
-//     public async void OnDelete_FalseValue_ShouldReturnFalse()
-//     {
-//         //Arrange
-//         _jr.Setup(x => x.Delete(1)).ReturnsAsync(false);
-//         //Act   
-//         var result = _js.Delete(1);
-//         //Assert
-//         Assert.False(await result);
-//     }
-//
-//     [Fact]
-//     public async void OnDelete_ValidValue_ShouldReturnTrue()
-//     {
-//         //Arrange
-//         var data = faker.Generate(1)[0];
-//         _jr.Setup(x => x.Delete(1)).ReturnsAsync(true);
-//         //Act   
-//         var result = _js.Delete(1);
-//         //Assert
-//         Assert.True(await result);
-//     }
-//     [Fact]
-//     public async void OnPut_FalseValue_ShouldReturnFalse()
-//     {
-//         //Arrange
-//         _jr.Setup(x => x.Put(It.IsAny<ApplicantDTO>())).ReturnsAsync(false);
-//         //Act   
-//         var result = _js.Put(new Applicant());
-//         //Assert
-//         Assert.False(await result);
-//     }
-//
-//     [Fact]
-//     public async void OnPut_ValidValue_ShouldReturnMappedObject()
-//     {
-//         //Arrange
-//         var data = faker.Generate(1)[0];
-//         _jr.Setup(x => x.Put(It.IsAny<ApplicantDTO>())).ReturnsAsync(true);
-//         var mockresult = new Applicant()
-//         {
-//             id = data.ProductId,
-//             description = data.Description,
-//             title = data.Title,
-//             maxPay = data.MaxPay,
-//             minPay = data.MinPay,
-//             subTitle = data.SubTitle
-//         };
-//         //Act   
-//         var result = _js.Put(mockresult);
-//         //Assert
-//         Assert.True(await result);
-//     }
-// }
+﻿using System.Linq;
+using AutoMapper;
+using Bogus;
+using FluentAssertions;
+using JobBoardRepository;
+using JobBoardRepository.Domain;
+using JobBoardRepository.Interface;
+using JobBoardServices;
+using JobBoardServices.Interface;
+using JobBoardServices.Profile;
+using JobBoardServices.Service;
+using JobBoardServices.View;
+using Moq;
+using Xunit;
+
+namespace JobBoardDemoTests.Services;
+
+public class ApplicantServiceTests
+{
+    private IApplicantService _js;
+    public Mock<IApplicantRepository> _ar;
+    public Mock<IJobApplicantRepository> _jar;
+    public Faker<ApplicantDTO> ApFaker;
+    public Faker<JobDTO> JobFaker;
+    public Mapper m ;
+    public ApplicantServiceTests()
+    {
+        
+        JobProfile myProfile = new JobProfile();
+        ApplicantProfile myProfile2 = new ApplicantProfile();
+        var configuration = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile(myProfile);
+            cfg.AddProfile(myProfile2);
+        });
+        
+        m = new Mapper(configuration);
+        this._jar = new Mock<IJobApplicantRepository>();
+        this._ar = new Mock<IApplicantRepository>();
+        this._js = new ApplicantService(_ar.Object,m,_jar.Object);
+        this.ApFaker = new Faker<ApplicantDTO>()
+            .RuleFor<int>(property:x=>x.Id,setter:y=>y.IndexFaker)
+            .RuleFor<string>(property:x=>x.Name,setter:y=>y.Name.FullName())
+            .RuleFor<double>(property:x=>x.WageExpectation,setter:y=>y.Finance.Random.Double());
+        this.JobFaker = new Faker<JobDTO>()
+            .RuleFor(property: x => x.ProductId, c => c.IndexFaker)
+            .RuleFor(property: x => x.Title, setter: c => c.Company + " " + c.Commerce)
+            .RuleFor(property: x => x.Description, setter: c => c.Lorem.Word())
+            .RuleFor(property: x => x.SubTitle, setter: c => c.Lorem.Word())
+            .RuleFor(property: x => x.MaxPay, setter: c => c.Finance.Random.Int())
+            .RuleFor(property: x => x.MinPay, setter: (c, usr) =>
+            {
+                int val;
+                do
+                {
+                    val = c.Finance.Random.Int();
+                } while (val > usr.MaxPay);
+
+                return val;
+            });
+    }
+
+    [Fact]
+    public async void OnGet_ValidValue_ShouldReturnMappedObject()
+    {
+        //Arrange
+        var data = ApFaker.Generate(20).ToArray();
+        var data2 = JobFaker.Generate(20).ToArray();
+        _ar.Setup(x => x.Get()).ReturnsAsync(data);
+        _jar.Setup(x => x.GetAppliedJobs(It.IsAny<int>())).Returns<int>(async x=>data2.Where(y=>y.ProductId==x).ToArray());
+        var mockresult = data.Select(x =>
+        {
+            return new Applicant()
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Jobs = data2.Where(y => y.ProductId == x.Id).Select(z => new Job()
+                {
+                    id= z.ProductId,
+                    description = z.Description,
+                    maxPay = z.MaxPay,
+                    minPay = z.MinPay,
+                    subTitle = z.SubTitle,
+                    title = z.Title
+                }).ToArray(),
+                WageExpectation = x.WageExpectation
+            };
+
+        }).ToArray();
+        //Act   
+        Applicant[] result = await _js.Get();
+        //Assert
+        result.Should().BeEquivalentTo(mockresult);
+    }
+    [Fact]
+    public async void OnPost_FalseValue_ShouldReturnFalse()
+    {
+        //Arrange
+        _ar.Setup(x => x.Post(null)).ReturnsAsync(false);
+        //Act   
+        var result = _js.Post(null);
+        //Assert
+        Assert.False( await result);
+    }
+
+    [Fact]
+    public async void OnPost_ValidValue_ShouldReturnMappedObject()
+    {
+        ;
+        //Arrange
+        var data = ApFaker.Generate(1)[0];
+        var data2 = JobFaker.Generate(1)[0];
+        _ar.Setup(x => x.Post(It.IsAny<ApplicantDTO>())).ReturnsAsync(true);
+        var mockresult = 
+             new Applicant()
+            {
+                Id = data.Id,
+                Name = data.Name,
+                WageExpectation = data.WageExpectation};
+        //Act   
+        var result = await _js.Post(mockresult);
+        mockresult.Jobs = new Job[]
+        {
+            new Job()
+            {
+                id = data2.ProductId,
+                description = data2.Description,
+                maxPay = data2.MaxPay,
+                minPay = data2.MinPay,
+                subTitle = data2.SubTitle,
+                title = data2.Title
+            }
+        };
+        //Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public async void OnGetId_ValidId_ShouldReturnMappedObject()
+    {
+        //Arrange
+        var data = ApFaker.Generate(1)[0];
+        var data2 = JobFaker.Generate(5);
+        _ar.Setup(x => x.GetId(1)).ReturnsAsync(data);
+        _jar.Setup(x => x.GetAppliedJobs(It.IsAny<int>())).Returns<int>(async x=>data2.ToArray());
+        var mockresult = new Applicant()
+        {
+            Id = data.Id,
+            Name = data.Name,
+            Jobs = data2.Select(x=>new Job()
+            {
+                id= x.ProductId,
+                description = x.Description,
+                maxPay = x.MaxPay,
+                minPay = x.MinPay,
+                subTitle = x.SubTitle,
+                title = x.Title
+            }).ToArray(),
+            WageExpectation = data.WageExpectation
+        };
+        //Act   
+        var result = await _js.GetId(1);
+        //Assert
+        result.Should().BeEquivalentTo(mockresult);
+    }
+    [Fact]
+    public async void OnDelete_FalseValue_ShouldReturnFalse()
+    {
+        //Arrange
+        _ar.Setup(x => x.Delete(1)).ReturnsAsync(false);
+        //Act   
+        var result = _js.Delete(1);
+        //Assert
+        Assert.False(await result);
+    }
+
+    [Fact]
+    public async void OnDelete_ValidValue_ShouldReturnTrue()
+    {
+        //Arrange
+        var data = ApFaker.Generate(1)[0];
+        _ar.Setup(x => x.Delete(1)).ReturnsAsync(true);
+        //Act   
+        var result = _js.Delete(1);
+        //Assert
+        Assert.True(await result);
+    }
+    [Fact]
+    public async void OnPut_FalseValue_ShouldReturnFalse()
+    {
+        //Arrange
+        _ar.Setup(x => x.Put(It.IsAny<ApplicantDTO>())).ReturnsAsync(false);
+        //Act   
+        var result = _js.Put(new Applicant());
+        //Assert
+        Assert.False(await result);
+    }
+
+    [Fact]
+    public async void OnPut_ValidValue_ShouldReturnMappedObject()
+    {
+        //Arrange
+        var data = ApFaker.Generate(1)[0];
+        var data2 = JobFaker.Generate(1)[0];
+        _ar.Setup(x => x.Put(It.IsAny<ApplicantDTO>())).ReturnsAsync(true);
+        var mockresult = new Applicant()
+        {
+            Id = data.Id,
+            Name = data.Name,
+            Jobs = new Job[]{new Job()
+            {
+                id= data2.ProductId,
+                description = data2.Description,
+                maxPay = data2.MaxPay,
+                minPay = data2.MinPay,
+                subTitle = data2.SubTitle,
+                title = data2.Title
+            }},
+            WageExpectation = data.WageExpectation
+        };
+        //Act   
+        var result = _js.Put(mockresult);
+        //Assert
+        Assert.True(await result);
+    }
+}
